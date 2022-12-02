efea1a74b18a29ca3ae8dd38e877ecefb7081b15
sonia-budhwar <budhwar1999sonia15@gmail.com>
2022-12-02 23:23:15 +0530

    commit

---
+++ palindrome.js
@@ -0,0 +1,14 @@
+let str="naman";
+let bag="";
+for(let i=str.length;i>=0;i--)
+{
+bag=bag+str[i];
+}
+if(bag==str)
+{
+console.log("not palindrome")
+}
+else
+{
+console.log("palindrome")
+}
---
+++ prime.js
@@ -0,0 +1,15 @@
+let number=90;
+let prime=true;
+for(let i=2;i<=num;i++)
+{
+   if(number==0)
+     prime=false;
+}
+if(prime)
+{
+   console.log("its prime")
+}
+else
+{
+console.log("its not prime")
+}   
